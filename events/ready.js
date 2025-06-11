a200288a948007d47b97ccce51347a5928d01ed2
_SharkTomato_ <142010343+07SharkTomato27@users.noreply.github.com>
2025-06-11 15:33:10 +0900

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,5 @@ const discord = require('discord.js');
 
 module.exports = (client) => {
     console.log(`Bot loaded on ${client.guilds.cache.size} server(s).`);
-    client.user.setActivity('Giving a bad time.');
+    client.user.setActivity('さいあくなめにあわせてやる');
 };
